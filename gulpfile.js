d90dcdcb0235f99a81a17bb4b6d76acbca591d24
Verthon <krzysiekksordyl@gmail.com>
2018-05-01 10:53:50 +0200

    fixed node-sass and gulp-sass issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 //import { rename } from 'fs';
 
 const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
 const rename = require('gulp-rename');
 const styleSrc = './src/scss/style.scss';
 const styleDist = './dist/css/';
 
 gulp.task('styles', ()=> {
   gulp.src(styleSrc)
+      .pipe(sass({
+        errorLogToConsole: true
+      }))
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest(styleDist));
 });
