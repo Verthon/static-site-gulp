19de4ca36ba71573a0770d001f393733d57c829e
Verthon <krzysiekksordyl@gmail.com>
2018-05-01 14:36:15 +0200

    file structure and gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 //import { rename } from 'fs';
 
 const gulp = require('gulp');
-const gulpSass = require('gulp-sass');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
 const styleSrc = './src/scss/style.scss';
 const styleDist = './dist/css/';
 
-gulp.task('styles', ()=> {
+gulp.task('styles', () => {
   gulp.src(styleSrc)
       .pipe(sass({
-        errorLogToConsole: true
+        errorLogToConsole: true,
+        outputStyle: 'compressed'
+      }))
+      .on('error', console.error.bind( console))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
       }))
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest(styleDist));
