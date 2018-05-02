276ec819bc7e1d5252583e33116aed0931f3de8c
Verthon <krzysiekksordyl@gmail.com>
2018-05-02 15:27:23 +0200

    added js support to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,40 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
-const styleSrc = './src/scss/style.scss';
-const styleDist = './dist/css/';
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('styles', () => {
+const styleSrc = 'src/scss/style.scss';
+const styleDist = './dist/css/';
+const styleWatch = 'src/scss/**/*.scss';
+
+const jsSrc = 'src/js/main.js';
+const jsDist = './dist/js/';
+const jsWatch = 'src/js/**/*.js';
+
+
+gulp.task('style', () => {
   gulp.src(styleSrc)
-      .pipe(sass({
-        errorLogToConsole: true,
-        outputStyle: 'compressed'
-      }))
-      .on('error', console.error.bind( console))
+      .pipe(sourcemaps.init())
+      //return gulp.src(styleSrc)
+      .pipe(sass()
+      .on('error', sass.logError))     
       .pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
       }))
       .pipe(rename({suffix: '.min'}))
+      .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest(styleDist));
+});
+
+gulp.task('js', () =>{
+  gulp.src(jsSrc)
+    .pipe(gulp.dest(jsDist));
+});
+
+gulp.task('default', ['style', 'js']);
+
+gulp.task('watch', ['default'], () => {
+  gulp.watch(styleWatch, ['style']);
+  gulp.watch(jsWatch, ['js']);
 });
